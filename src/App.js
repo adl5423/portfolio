5fea6410bfa82bf0af5b5315b281a31c60fc4f1e
Aisen Lopez <aisenlopez@hotmail.com>
2021-11-02 19:27:35 -0400

    Scroll to top component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
-import logo from './logo.svg';
+//import logo from './logo.svg';
 import './App.scss';
 import Navbar from './components/Navbar';
 import Home from './components/pages/Home';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Footer from './components/Footer';
+import ScrollButton from './components/ScrollButton';
 
 function App() {
   return (
@@ -16,6 +17,7 @@ function App() {
           <Route path='/' exact />
         </Switch>
         <Footer />
+        <ScrollButton />
       </Router>
     </>
   );
---
+++ src/components/Styles.js
@@ -0,0 +1,24 @@
+import styled from 'styled-components';
+
+export const Heading = styled.h1`
+text-align: center;
+color: green;
+`;
+
+export const Content = styled.div`
+overflowY: scroll;
+height: 2500px;
+`;
+
+export const Button = styled.div`
+position: absolute;
+display: flex;
+justify-content: center;
+width: 100%;
+bottom: -525px;
+height: 0;
+font-size: 3rem;
+z-index: 1;
+cursor: pointer;
+color: white;
+`
