62a39ebe882d06ef4376a0dfe84f4b1ae5a9958f
Aisen Lopez <aisenlopez@hotmail.com>
2021-11-30 15:57:56 -0500

    about me 2 updated (no responsiveness)

--- src/components/BiographyTwo.js
+++ src/components/BiographyTwo.js
@@ -4,6 +4,12 @@ import './BiographyTwo.scss';
 function BiographyTwo() {
     return (
         <div>
+            {/* <div className="wave-back">
+                <div class="wave wave1" />
+                <div class="wave wave2" />
+                <div class="wave wave3" />
+                <div class="wave wave4" />
+            </div> */}
             <div className="two-container">
                 <div className="info-container">
                     <div className="skills-container">
@@ -52,8 +58,39 @@ function BiographyTwo() {
                             </h3>
                         </div>
                     </div>
-                    <div classname="bio-container">
-
+                    <div className="exp-container">
+                        <h2 className="exp-title">
+                            Biography
+                        </h2>
+                        <p className="exp-desc">
+                            Aisen D. Lopez Jr. is a Software Engineering Apprentice with vast knowledge in Web Design, Web Development Project Manager, and Graphic Designer. Since he was twelve years old, Aisen has been creating awesome projects, initially with simple drag-and-drop tools and subsequently with more challenging tools. In 2014, Aisen accepted the challenge of founding and managing a small e-business on which he provided graphic design and marketing techniques services to numerous retailing and service businesses. After graduating from high school and recieving the Eagle Scout Award in 2018, Aisen accepted an offer from Penn State University to join the Aerospace Engineering program, in which he acquired mathematical, computer-aided designing, data science,and management knowledge in Aviation and Technology. In 2019, Aisen started working as Computer Lab Assistant at Penn State Abington, and later as Technical Professor Assistant, roles in which he leaded team members in various tasks and processes, performed customer service and troubleshooting, and assisted faculty in the use of physical and remote technologies. After being impacted by the COVID-19 Pandemic and re-encountering with his software and web development passion in 2020, Aisen transferred into the Software Engineering program at Penn State University. In 2021, Aisen became a Software Engineer Intern at Red Ventures, where he mastered web design and was exposed to project management techniques, which leaded Aisen to becoming Web Development Project Manager for the Online and Information Technology department at Penn State Abington.
+                        </p>
+                    </div>
+                    <div className="mission-vision-container">
+                        <div className="mission-container">
+                            <h2 className="mission-title">
+                                Mission
+                            </h2>
+                            <h3 className="mission-desc">
+                                To transform an object or incomplete idea into a solution, and then shape it in a unique reality, which will always be in constant improvement.
+                            </h3>
+                        </div>
+                        <div className="vision-container">
+                            <h2 className="vision-title">
+                                Vision
+                            </h2>
+                            <h3 className="vision-desc">
+                                To be part of a simplified, yet versatile environment, on which I can contribute solutions to any task that might be seen as complex.
+                            </h3>
+                        </div>
+                        <div className="purpose-container">
+                            <h2 className="purpose-title">
+                                Purpose
+                            </h2>
+                            <h3 className="purpose-desc">
+                                To become the best version of myself, guide others into success, and influence the path towards the industry future.
+                            </h3>
+                        </div>
                     </div>
                 </div>
             </div>
