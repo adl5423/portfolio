3faecbcfe3d08ceffa532e5b61ec986c7420e50f
Aisen Lopez <aisenlopez@hotmail.com>
2021-12-03 21:28:13 -0500

    overlays prototypes

--- src/components/ProjectBlock.js
+++ src/components/ProjectBlock.js
@@ -19,21 +19,43 @@ function ProjectBlock() {
                 </h1>
                 <div className="photo-container">
                     <Link to={{ pathname: "https://www.myslumberyard.com/in-the-news" }} target="_blank">
-                        <img src={Press} alt="press-room" className="screenshots"></img>
+                        <div className="image">
+                            <img src={Press} alt="press-room" className="screenshots"></img>
+                            <div className="screenshots_overlay">
+                                <h2 className="screenshots_context">
+                                    My Slumber Yard Press Page
+                                </h2>
+                                <p className="screenshots_context-desc">
+                                    Internship project; complete redesign of My Slumber Yard's Press Page. HTML, JS, SCSS, PHP.
+                                </p>
+                            </div>
+                        </div>
                     </Link>
                     <Link to={{ pathname: "https://github.com/adl5423/omdb-case-competition" }} target="_blank">
-                        <img src={Omdb} alt="movie database" className="screenshots"></img>
-                        <div className="overlay">
-                            <h2 className="context">
-                                Movie database Webapp
-                            </h2>
-                            <p className="context-desc">
-                                Internship team case competition consisting of creating an IMDB prototype using open source database.
-                            </p>
+                        <div className="image">
+                            <img src={Omdb} alt="movie database" className="screenshots"></img>
+                            <div className="screenshots_overlay">
+                                <h2 className="screenshots_context">
+                                    Movie Database Web App
+                                </h2>
+                                <p className="screenshots_context-desc">
+                                    Internship team case competition; creation of IMDB prototype using open source database. React.js, Redux, JQuery, RestAPI, HTML, SCSS, JS.
+                                </p>
+                            </div>
                         </div>
                     </Link>
                     <Link to={{ pathname: "https://sites.psu.edu/abid" }} target="_blank">
-                        <img src={Abid} alt="abid" className="screenshots"></img>
+                        <div className="image">
+                            <img src={Abid} alt="abid" className="screenshots"></img>
+                            <div className="screenshots_overlay">
+                                <h2 className="screenshots_context">
+                                    PSU Abington OIT Webpage
+                                </h2>
+                                <p className="screenshots_context-desc">
+                                    Project manager for Freshman and Sophomore students; redesign of various pages. WordPress Divi Builder, HTML, CSS.
+                                </p>
+                            </div>
+                        </div>
                     </Link>
                 </div>
             </div>
